b03f2a0ed4f031af98fb71edf2ef7290c551b6b7
Alberto Liguori <a1liguorialberto@gmail.com>
2020-11-03 16:48:51 +0100

    Now slides on keyboard input

--- main.js
+++ main.js
@@ -1,29 +1,86 @@
 $(document).ready(function(){
 
+  //click input
+  $(".prev").click(function(){
+    var activeImgEl = $("img.active")
+    activeImgEl.removeClass("active")
+    if(activeImgEl.hasClass("first")){
+      $("img.last").addClass("active")
 
-$(".prev").click(function(){
-  var activeImgEl = $("img.active")
-  activeImgEl.removeClass("active")
-  if(activeImgEl.hasClass("first")){
-    $("img.last").addClass("active")
+    }else{
+      activeImgEl.prev().addClass("active")
+    }
 
-  }else{
-    activeImgEl.prev().addClass("active")
-  }
+  })
+
+
+  $(".next").click(function(){
+    var activeImgEl = $("img.active")
+    activeImgEl.removeClass("active")
+    if(activeImgEl.hasClass("last")){
+      $("img.first").addClass("active")
+
+    }else{
+      activeImgEl.next().addClass("active")
+    }
+
+  })
+
+
+
+
+
+  //Arrow input
+  document.addEventListener("keydown", function(event) {
+    var activeImgEl = $("img.active")
+
+    if(event.which == 37){
+
+
+      activeImgEl.removeClass("active")
+
+      if(activeImgEl.hasClass("first")){
+        $("img.last").addClass("active")
+
+      }else{
+        activeImgEl.prev().addClass("active")
+      }
+
+    }
+    else if(event.which == 39){
+
+      activeImgEl.removeClass("active")
+
+      if(activeImgEl.hasClass("last")){
+        $("img.first").addClass("active")
+
+      }else{
+        activeImgEl.next().addClass("active")
+      }
+
+    }
+
+
+  })
 
 })
 
 
-$(".next").click(function(){
-  var activeImgEl = $("img.active")
-  activeImgEl.removeClass("active")
-  if(activeImgEl.hasClass("last")){
-    $("img.first").addClass("active")
-
-  }else{
-    activeImgEl.next().addClass("active")
-  }
-
+document.addEventListener("keydown", function(event) {
+  console.log(event.which);
 })
 
-})
+
+
+
+// $(document).bind('keydown', 'left', function() {
+//   var activeImgEl = $("img.active")
+//   activeImgEl.removeClass("active")
+//   if(activeImgEl.hasClass("first")){
+//     $("img.last").addClass("active")
+//
+//   }else{
+//     activeImgEl.prev().addClass("active")
+//   }
+//
+//
